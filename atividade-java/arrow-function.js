b53b7c5972da0209c81310b8874765e1d647bfde
jayling159 <162716502+jayling159@users.noreply.github.com>
2024-03-27 08:41:14 -0300

    Update arrow-function.js

--- atividade-java/arrow-function.js
+++ atividade-java/arrow-function.js
@@ -1,9 +1,9 @@
 function apresentar(nome) {
-  return `meu nome é ${jared}`;
+  return `meu nome é ${nome}`;
 }
 
 // Arrow function
-const apresentarArrow = nome => `meu nome é ${jared}`;
+const apresentarArrow = nome => `meu nome é ${nome}`;
 const soma = (num1, num2) => num1 + num2;
 
 //Arrow function com + de 1 linha de instrução
